255d7b056aaec943318433c3f8b659751fa6f40a
LeSanglier <lesanglierdesardennes@gmail.com>
2014-11-02 14:43:18 +0100

    Add link

--- lien.inc.php
+++ lien.inc.php
@@ -4,6 +4,7 @@
                         <ul>
                             <li><a href="http://www.arduino.cc/">Site officiel Arduino</a>
                             <li><a href="http://hacklab.fr">Hacklab/ILArd</a>
+                            <li><a href="https://github.com/Nekrofage/prezArduino">Repository Github de la présentation</a></li>
                         </ul>
 					</p>
 
